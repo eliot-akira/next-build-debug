21596c03f24f3219736e743affdd8f48258aca44
eliot-akira <me@eliotakira.com>
2017-07-12 11:53:31 +0200

    Add failing/working comparison

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
+
+// This snippet breaks the build
+
 for (let i = 0; i < 1; i++) {
+  const g = () => {}
+  const b = () => g()
+  i = 2
+}
+
+// This works
+
+let i
+
+for (i = 0; i < 1; i++) {
   const g = () => {}
   const b = () => g()
   i = 2
